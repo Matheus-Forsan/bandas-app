8b07ae64fd9be205004ffbf524860f235c374a97
matheus marques <matheusforsan@gmail.com>
2024-03-28 09:01:18 -0300

    bandas

---
+++ src/screens/Home.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { View, Text ,StyleSheet,StatusBar} from "react-native";
+import MusicItem from "../components/MusicItem";
+
+export default function Home({navigation}) {
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+  const item = {
+    id:1,
+    title: "Feel Good Inc.",
+    group: "Gorillaz",
+    album_image:"https://th.bing.com/th/id/R.644ab768a52d09cc162c620ab769ef46?rik=qS72fq8XfJMwBg&pid=ImgRaw&r=0"
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#121212" />
+      <Text style={styles.title}>Minhas Músicas</Text>
+      <MusicItem
+      isPlaying={() => currentPlaying == item.id}
+      music={item}
+      navigation={navigation}
+      onPlayPause={() => {}}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "#121212",
+    paddingTop: 16,
+
+  },
+  title:{
+    fontSize:36,
+    fontWeight:"bold",
+    color:"#fff",
+    marginBottom:20,
+    marginLeft:20,
+  }
+});
